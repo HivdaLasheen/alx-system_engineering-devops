33a47f84b539ac05f2752d31472f3324ce226f69
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-28 23:46:30 +0000

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { 'school':
-ensure  => 'present',
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
