96ebdf0dcce17428808f039af397bdcf31b59fb6
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-28 23:55:07 +0000

    kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,10 @@
-exec{'pkill -f killmenow':
-path  => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'kill_process':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+}
+
+# Notify the exec resource to run
+notify { 'trigger_kill':
+  notify => Exec['kill_process'],
 }
